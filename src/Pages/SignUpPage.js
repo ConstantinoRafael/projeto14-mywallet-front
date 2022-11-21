7960c570f8d106e650e17a04f5fb4b6203e0f3d3
ConstantinoRafael <rafael.constantino89@gmail.com>
2022-11-21 02:07:09 -0300

    style: sign-up page

--- src/Pages/SignUpPage.js
+++ src/Pages/SignUpPage.js
@@ -1,4 +1,4 @@
-//import styled from "styled-components";
+import styled from "styled-components";
 import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
@@ -28,35 +28,97 @@ export default function SignUpPage() {
   }
 
   return (
-    <>
-      <h1>MyWallet</h1>
-      <form onSubmit={sendData}>
-        <input
+    <SignUp>
+      <Logo>MyWallet</Logo>
+      <Form onSubmit={sendData}>
+        <Input
           placeholder="Nome"
           type="text"
           onChange={(e) => setName(e.target.value)}
-        ></input>
-        <input
+        ></Input>
+        <Input
           placeholder="E-mail"
           type="email"
           onChange={(e) => setEmail(e.target.value)}
-        ></input>
-        <input
+        ></Input>
+        <Input
           placeholder="Senha"
           type="password"
           onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <input
+        ></Input>
+        <Input
           placeholder="Confirme a senha"
           type="password"
           onChange={(e) => setConfirmPassword(e.target.value)}
-        ></input>
-        <button type="submit">Cadastrar</button>
-      </form>
+        ></Input>
+        <Button type="submit">Cadastrar</Button>
+      </Form>
 
       <Link to={"/"}>
         <p>Já tem uma conta? Entre agora!</p>
       </Link>
-    </>
+    </SignUp>
   );
 }
+
+const SignUp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    text-decoration: none;
+  }
+
+  p{
+    color:#ffffff;
+    font-size: 15px;
+    font-weight: 700;
+  }
+`;
+
+const Logo = styled.h1`
+  color: #ffffff;
+  font-size: 32px;
+  margin-top: 159px;
+  margin-bottom: 24px;
+  font-family: "Saira Stencil One", cursive;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  
+`
+
+const Input = styled.input`
+  width: 326px;
+  height: 58px;
+  background-color: #ffffff;
+  color: #000000;
+  font-size: 20px;
+  border-radius: 5px;
+  border-style: solid;
+  padding-left: 10px;
+  margin-bottom: 13px;
+
+   ::placeholder{
+    color: #000000;
+    font-size: 20px;
+    font-weight: 400;
+   }
+`
+
+const Button = styled.button`
+  width: 346px;
+  height: 46px;
+  border-radius: 5px;
+  border:none;
+  background-color: #A328D6;
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 36px;
+
+`
